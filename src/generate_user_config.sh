9ab2ebf1fdd0be9d9668efbb9e40319dd24be7f9
kukv <rio.by.kukv@gmail.com>
2024-09-22 16:22:33 +0900

    パスワードをハッシュ化する

--- src/generate_user_config.sh
+++ src/generate_user_config.sh
@@ -6,10 +6,16 @@ CREDENTIAL_FILE="/credentials/${SERVER_NAME}.yaml"
 TEMPLATE_FILE="/templates/user-data.j2"
 GENERATED_FILE="/generated/${SERVER_NAME}/user-data"
 
+root_password=$(openssl passwd -6 -salt=salt $(yq ".users.root.password" "${CREDENTIAL_FILE}"))
+operation_user_password=$(openssl passwd -6 -salt=salt $(yq ".users.operation_user.password" "${CREDENTIAL_FILE}"))
+
 if [[ ${SERVER_NAME} =~ ^.*node.+$ ]]; then
   export GENERATE_TARGET="node"
 else
   export GENERATE_TARGET="gateway"
 fi
 
-jinja2 --format=yaml ${TEMPLATE_FILE} ${CREDENTIAL_FILE} > ${GENERATED_FILE}
+jinja2 --format=yaml ${TEMPLATE_FILE} ${CREDENTIAL_FILE} \
+      -D "root_password=${root_password}" \
+      -D "operation_user_password=${operation_user_password}" \
+      > ${GENERATED_FILE}
